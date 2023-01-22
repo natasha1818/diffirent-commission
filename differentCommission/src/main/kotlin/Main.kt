a66c9d2ecdac8b89ff6867c9b5441cdd0ee13e74
Natasha K <k_natasha1818@mail.ru>
2023-01-22 11:48:25 +0700

    modification

--- differentCommission/src/main/kotlin/Main.kt
+++ differentCommission/src/main/kotlin/Main.kt
@@ -1,15 +1,16 @@
 fun main() {
     val transfer = transferPayment(12_000.0)
     val transfer_2 = transferPayment(12_000.0, typeCard = "Maestro", amountOfTransfer = 65_000.0)
+    val transfer_3 = transferPayment(12_000.0, typeCard = "Мир")
 }
 
 fun transferPayment(amount: Double, typeCard: String = "VK Pay", amountOfTransfer: Double = 0.0) {
     when (typeCard) {
         "VK Pay" -> println("Сумма вашего первода $amount рублей")
         "Maestro", "MasterCard" -> calculationAmount(amount, amountOfTransfer)
+        "Мир","Visa"-> calculationAmountMirAndVisa(amount,amountOfTransfer)
     }
 }
-
 fun calculationAmount(amount: Double, amountOfTransfer: Double) {
     val totalAmount = amount + amountOfTransfer
     val commission = amount * 0.006 + 20
@@ -21,4 +22,18 @@ fun calculationAmount(amount: Double, amountOfTransfer: Double) {
             общая сумма перевода $totalSum  рублей""".trimMargin()
         )
     }
+}
+fun calculationAmountMirAndVisa(amount: Double,amountOfTransfer: Double){
+    if (amount < 35){
+        println("Ваша сумма меьнше минимальной суммы перевода 35 рублей.")
+    }else{
+        val commission = amount*0.0075
+        val totalAmount = amount + commission
+        println(
+            """Сумма вашего первода $amount и дополнительная комиссия $commission, 
+            общая сумма перевода $totalAmount  рублей""".trimMargin()
+        )
+
+    }
+
 }
